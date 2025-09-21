d8580ce030b877dbafb20903191980778db3e4bb
ibruhhim <ibrahimellahi24@gmail.com>
2025-09-21 00:46:34 -0400

    fixed hero pfp

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -35,7 +35,7 @@ const Hero = () => {
 
       <div className='flex items-center justify-center gap-36 w-full flex-wrap'>
         <div className='w-72 h-72 sm:w-80 sm:h-80 shadow-[0_0_20px_lightblue] overflow-hidden rounded-full'>
-          <div className='w-80 h-80 border-blue-400 border-2 rounded-full mr-20 overflow-hidden flex justify-self-center items-center'>
+          <div className='w-72 h-72 sm:w-80 sm:h-80 border-blue-400 border-2 rounded-full mr-20 overflow-hidden flex justify-self-center items-center'>
             <Image
               src={pfp}
               alt="My Photo"

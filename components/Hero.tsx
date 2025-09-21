9295cd6918121a7463abcfefad0b332593a714e9
ibruhhim <ibrahimellahi24@gmail.com>
2025-09-21 01:30:38 -0400

    pfp responsive now

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -34,12 +34,12 @@ const Hero = () => {
       </div>
 
       <div className='flex items-center justify-center gap-36 w-full flex-wrap'>
-        <div className='w-72 h-72 sm:w-80 sm:h-80 shadow-[0_0_20px_lightblue] overflow-hidden rounded-full'>
-          <div className='w-72 h-72 mr-30 sm:w-80 sm:h-80 border-blue-400 border-2 rounded-full overflow-hidden flex justify-self-center items-center'>
+        <div className='relative w-80 h-80 shadow-[0_0_20px_lightblue] overflow-hidden rounded-full'>
+          <div className='absolute w-80 h-80 right-10 border-blue-400 border-2 rounded-full overflow-hidden flex justify-self-center items-center'>
             <Image
               src={pfp}
               alt="My Photo"
-              className='object-cover w-full h-full object-left sm:object-center'
+              className='object-cover w-full h-full'
             />
           </div>
         </div>
